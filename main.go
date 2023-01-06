d03e5b5ce913334700e66da254c312b72a4c6a12
traseta <traseta@gmail.com>
2023-01-06 20:05:27 +0100

    Get reader. Add gui window.

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 
+	"fyne.io/fyne/v2/app"
 	"github.com/sf1/go-card/smartcard"
 )
+
+var reader *smartcard.Reader
 
 func main() {
 
@@ -14,6 +17,37 @@ func main() {
 		fmt.Println("Error establishing context")
 		return
 	}
+	// get readers list
+	readers_list, err := context.ListReadersWithCard()
+	if err != nil {
+		return
+	}
+
+	// handle list of readers & get card
+	if len(readers_list) == 0 {
+		fmt.Println("\nplease insert smart card\n")
+		return
+	}
+
+	// work with one reader for now
+	if len(readers_list) == 1 {
+		reader = readers_list[0]
+	}
+
+	// connect to reader
+	card, err := reader.Connect()
+	if err != nil {
+		panic(err)
+	}
+
+	// fmt.Printf("%T\n------------------------------------\n", card)
+
+	defer card.Disconnect()
 
 	defer context.Release()
+
+	myApp := app.New()
+	windw := myApp.NewWindow("GoSteel")
+
+	windw.ShowAndRun()
 }
